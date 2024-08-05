f7b3e0dd8b2de2aa0c5f3d42a6470c30f2fe48c4
requisz <prk3479@gmail.com>
2024-08-04 23:23:49 -0400

    background fix

--- components/AnimatedBackground.js
+++ components/AnimatedBackground.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../styles/animatedBackground.css';
 
 const AnimatedBackground = () => {
-  return <div></div>; // No need for extra elements, background is handled by CSS
+  return null;
 };
 
 export default AnimatedBackground;
